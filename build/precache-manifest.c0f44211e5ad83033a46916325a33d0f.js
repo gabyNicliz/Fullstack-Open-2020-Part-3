50706518a40b0e5127f6e1a420b5d8dde720cb4f
gabyNicliz <gabynicliz93@gmail.com>
2020-04-20 21:28:53 -0300

    uibuild

---
+++ build/precache-manifest.c0f44211e5ad83033a46916325a33d0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba9da93e2c7aae5ba8ec6e454b3d3e34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "317a475a2b82276c3e61",
+    "url": "/static/css/main.c3b58b01.chunk.css"
+  },
+  {
+    "revision": "fb4cca7c826df130d13f",
+    "url": "/static/js/2.91d2cd84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.91d2cd84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "317a475a2b82276c3e61",
+    "url": "/static/js/main.af85962e.chunk.js"
+  },
+  {
+    "revision": "2707f5a550e5129b2153",
+    "url": "/static/js/runtime-main.69f5a8ea.js"
+  }
+]);
