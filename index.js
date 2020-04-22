2a4e7a65c1d69fa9c2c05f213e75f0c28b2b708d
gabyNicliz <gabynicliz93@gmail.com>
2020-04-22 20:53:36 -0300

    uibuild

--- index.js
+++ index.js
@@ -40,31 +40,45 @@ let persons = [
   }
 ]
 
+// get all persons from the the phonebook's database
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
     response.json(persons.map(person => person.toJSON()))
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person.toJSON())
+// get a particular person based on their id the phonebook's database
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+  .then(person => {
+    if (person) {
+      response.json(person.toJSON())
+    } else {
+      response.status(404).end()
+    }
+  })
+  .catch(error => next(error))
+})
+
+// show how many persons are in the phonebook's database
+app.get('/api/info', (request, response) => {
+  Person.find({}).then(persons => {
+    const numberOfPersons = persons.length
+    const date = new Date()
+    response.send(`Phonebook has info for ${numberOfPersons} people\n${date}`)
   })
 })
 
-app.get('/api/info', (request, response) => {
-  const numberOfPersons = persons.length
-  const date = new Date()
-  response.send(`Phonebook has info for ${numberOfPersons} people\n${date}`)
+// delete person from the phonebook's database
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(p => p.id !== id)
-
-  response.status(204).end()
-})
-
+// post new person's info on the phonebook's database
 app.post('/api/persons/', (request, response) => {
   const body = request.body
 
@@ -99,12 +113,46 @@ app.post('/api/persons/', (request, response) => {
   app.use(morgan(' :method :url :status :response-time ms - :res[content-length] :body'))
 })
 
+// update person's number from the phonebook's database
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  .then(updatedPerson => {
+    response.json(updatedPerson.toJSON())
+  })
+  .catch(error => next(error))
+})
+
 const generateId = () => {
   const min = persons.length
   const max = 10000
   return Math.trunc(Math.random() * (max - min) + min)
 }
 
+
+const unknownEndpoint = (request, response) => {
+  return response.status(400).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+    return response.status(404).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 
